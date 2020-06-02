adc3258aaf98cbb1ddbe6fefc21c006608ca2236
AlexeyMarg <65703985+AlexeyMarg@users.noreply.github.com>
2020-06-02 11:09:28 +0300

    Update lsys_gui_tkinter.py

--- lsys_gui_tkinter.py
+++ lsys_gui_tkinter.py
@@ -4,7 +4,6 @@ import tkinter as tk
 class lsys_gui:
     def __init__(self, parent):
         self.parent = parent
-        self.state = 'css'
         self.init_form()
         self.init_commands()
 
@@ -14,33 +13,47 @@ class lsys_gui:
 
         def drawABCD():
             self.frame_entry_labels = tk.Frame(self.parent)
-            self.frame_entry_labels.pack(side='left')
-            self.labelA = tk.Label(self.frame_entry_labels, text='A = ').pack()
-            self.labelB = tk.Label(self.frame_entry_labels, text='B = ').pack()
-            self.labelC = tk.Label(self.frame_entry_labels, text='C = ').pack()
-            self.labelD = tk.Label(self.frame_entry_labels, text='D = ').pack()
+            self.frame_entry_labels.pack(side='left', anchor='n')
+            self.labelA = tk.Label(self.frame_entry_labels,
+                                   text='A = ').pack()
+            self.labelB = tk.Label(self.frame_entry_labels,
+                                   text='B = ').pack()
+            self.labelC = tk.Label(self.frame_entry_labels,
+                                   text='C = ').pack()
+            self.labelD = tk.Label(self.frame_entry_labels,
+                                   text='D = ').pack()
 
             self.frame_entry = tk.Frame(self.parent)
-            self.frame_entry.pack(side='left')
-            self.entryA = tk.Entry(self.frame_entry, width=30).pack(padx=2, pady=2)
-            self.entryB = tk.Entry(self.frame_entry, width=30).pack(padx=2, pady=2)
-            self.entryC = tk.Entry(self.frame_entry, width=30).pack(padx=2, pady=2)
-            self.entryD = tk.Entry(self.frame_entry, width=30).pack(padx=2, pady=2)
+            self.frame_entry.pack(side='left', anchor='n')
+            self.entryA = tk.Entry(self.frame_entry,
+                                   width=30).pack(padx=2, pady=2)
+            self.entryB = tk.Entry(self.frame_entry,
+                                   width=30).pack(padx=2, pady=2)
+            self.entryC = tk.Entry(self.frame_entry,
+                                   width=30).pack(padx=2, pady=2)
+            self.entryD = tk.Entry(self.frame_entry,
+                                   width=30).pack(padx=2, pady=2)
 
         def drawTF():
             self.frame_entry_labels = tk.Frame(self.parent)
-            self.frame_entry_labels.pack(side='left')
-            self.labelNum = tk.Label(self.frame_entry_labels, text='Numerator ').pack(anchor='w')
-            self.labelDen = tk.Label(self.frame_entry_labels, text='Denominator ').pack()
+            self.frame_entry_labels.pack(side='left', anchor='n')
+            self.labelNum = tk.Label(self.frame_entry_labels,
+                                     text='Numerator ').pack(anchor='w')
+            self.labelDen = tk.Label(self.frame_entry_labels,
+                                     text='Denominator ').pack()
 
             self.frame_entry = tk.Frame(self.parent)
-            self.frame_entry.pack(side='left')
-            self.entryNum = tk.Entry(self.frame_entry, width=30).pack(padx=2, pady=2)
-            self.entryDen = tk.Entry(self.frame_entry, width=30).pack(padx=2, pady=2)
+            self.frame_entry.pack(side='left', anchor='n')
+            self.entryNum = tk.Entry(self.frame_entry,
+                                     width=30).pack(padx=2, pady=2)
+            self.entryDen = tk.Entry(self.frame_entry,
+                                     width=30).pack(padx=2, pady=2)
 
         def draw_discrete():
-            self.labelT = tk.Label(self.frame_entry_labels, text='T= ').pack(anchor='w')
-            self.entryT = tk.Entry(self.frame_entry, width=5).pack(padx=2, pady=2)
+            self.labelT = tk.Label(self.frame_entry_labels,
+                                   text='T= ').pack(anchor='w')
+            self.entryT = tk.Entry(self.frame_entry,
+                                   width=5).pack(padx=2, pady=2)
 
         def select_mode():
             self.frame_entry_labels.destroy()
@@ -53,33 +66,62 @@ class lsys_gui:
                     draw_discrete()
 
         frame_cd = tk.Frame()
-        frame_cd.pack(side='left')
+        frame_cd.pack(side='left', anchor='n')
         self.cd = tk.BooleanVar()
         self.cd.set(0)
-        self.radioc = tk.Radiobutton(frame_cd, text='Continuous', variable=self.cd, value=0,
-                                     command=select_mode)
-        self.radioc.pack(anchor='nw')
-        self.radiod = tk.Radiobutton(frame_cd, text='Discrete', variable=self.cd, value=1,
-                                     command=select_mode)
-        self.radiod.pack(anchor='nw')
+        self.radioc = tk.Radiobutton(frame_cd,
+                                     text='Continuous',
+                                     variable=self.cd,
+                                     value=0,
+                                     command=select_mode).pack(anchor='nw')
+
+        self.radiod = tk.Radiobutton(frame_cd,
+                                     text='Discrete',
+                                     variable=self.cd,
+                                     value=1,
+                                     command=select_mode).pack(anchor='nw')
 
         frame_sstf = tk.Frame()
-        frame_sstf.pack(side='left')
+        frame_sstf.pack(side='left', anchor='n')
         self.sstf = tk.BooleanVar()
         self.sstf.set(0)
-        self.radioss = tk.Radiobutton(frame_sstf, text='State space', variable=self.sstf, value=0,
-                                     command=select_mode)
-        self.radioss.pack(anchor='nw')
-        self.radiotf = tk.Radiobutton(frame_sstf, text='Transfer function', variable=self.sstf, value=1,
-                                     command=select_mode)
-        self.radiotf.pack(anchor='nw')
-
+        self.radioss = tk.Radiobutton(frame_sstf,
+                                      text='State space',
+                                      variable=self.sstf,
+                                      value=0,
+                                      command=select_mode).pack(anchor='nw')
+        self.radiotf = tk.Radiobutton(frame_sstf,
+                                      text='Transfer function',
+                                      variable=self.sstf,
+                                      value=1,
+                                      command=select_mode).pack(anchor='nw')
         drawABCD()
 
+        self.label_result = tk.Label(justify='left', font=('Arial', '10'),
+                                     text='Stability: \n'
+                                     'Observability: \n'
+                                     'Controllability: \n'
+                                     'Overshoot: \n'
+                                     'Poles: \n'
+                                     'Transient time: \n'
+                                     'Oscillation coefficient: \n'
+                                     'Damping coefficient: ')
+        self.label_result.place(x=10, y=120)
+
+        self.bcalc = tk.Button(text='Calculate')
+        self.bcalc.place(x=10, y=60)
+
+        self.bplot = tk.Button(text='Plot', width=9)
+        self.bplot.place(x=100, y=60)
+
     def init_commands(self):
-        pass
+        def bcalc_pressed(event):
+            pass
+
+        self.bcalc.bind(bcalc_pressed)
 
 
 root = tk.Tk()
+root.geometry('500x300')
 my_gui = lsys_gui(root)
 root.mainloop()
